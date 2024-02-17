53d0c24e9e73cf34adf47f76d31a32031dc76ba2
ricardoalexandrejs <ricardoaj.designer@gmail.com>
2024-02-17 15:52:45 -0300

    adicionado o IF

--- scripts.js
+++ scripts.js
@@ -3,7 +3,12 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value) //arredonda o numero para cima (teto)
     const max = Math.floor(document.querySelector(".input-max").value) //arredonda o numero para baixo (chão)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    alert("o numero sorteado é " + result)
+    if (min >= max) {
+        alert("O primeiro numero tem de ser menor que o segundo número")
+
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+        alert("o numero sorteado é " + result)
+    }
 
 }
